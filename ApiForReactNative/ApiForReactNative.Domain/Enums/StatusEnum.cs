122d49166120e234f39c3af496928e2efad9213b
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2025-04-05 15:31:32 +0300

    enum eklendi.....

---
+++ ApiForReactNative/ApiForReactNative.Domain/Enums/StatusEnum.cs
@@ -0,0 +1,10 @@
+﻿using Ardalis.SmartEnum;
+public sealed class StatusEnum : SmartEnum<StatusEnum>
+{
+    public static readonly StatusEnum Beklemede = new("Beklemede", 1);
+    public static readonly StatusEnum Kapali = new("Kapali", 2);
+    public static readonly StatusEnum Aktif = new("Aktif", 3);
+    public StatusEnum(string name, int value) : base(name, value)
+    {
+    }
+}
